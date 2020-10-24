9e050955094a2e18f142f3cde21e2b1a5384a3ba
Mnunisa <71170873+Mnunisa@users.noreply.github.com>
2020-10-24 13:24:36 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            
+            'restitution':1.2,
+            'friction':0.001,
+            'density':0.01
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        //translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+}
